85e02997230053ba0af12aacef54d248b2235610
Nico Mexis <nico.mexis@kabelmail.de>
2025-12-16 07:13:30 +0100

    Update to 1.21.11

--- common/src/main/java/itemtransformhelper/ItemCamera.java
+++ common/src/main/java/itemtransformhelper/ItemCamera.java
@@ -1,14 +1,9 @@
 package itemtransformhelper;
 
-import java.util.function.Consumer;
 import net.minecraft.core.registries.Registries;
-import net.minecraft.network.chat.Component;
+import net.minecraft.resources.Identifier;
 import net.minecraft.resources.ResourceKey;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.TooltipFlag;
-import net.minecraft.world.item.component.TooltipDisplay;
 
 import static itemtransformhelper.ItemTransformHelper.MOD_ID;
 
@@ -24,17 +19,8 @@ public class ItemCamera extends Item {
 
     public ItemCamera() {
         super(new Properties()
-                .setId(ResourceKey.create(Registries.ITEM, ResourceLocation.fromNamespaceAndPath(MOD_ID, ID)))
+                .setId(ResourceKey.create(Registries.ITEM, Identifier.fromNamespaceAndPath(MOD_ID, ID)))
                 .stacksTo(1).arch$tab(StartupCommon.ITH_ITEM_GROUP));
-    }
-
-    @Override
-    public void appendHoverText(ItemStack itemStack, TooltipContext tooltipContext, TooltipDisplay tooltipDisplay,
-                                Consumer<Component> consumer, TooltipFlag tooltipFlag) {
-        consumer.accept(Component.literal("1) Place the camera in your hotbar"));
-        consumer.accept(Component.literal("2) Hold an item in your hand"));
-        consumer.accept(Component.literal("3) Use the cursor keys to"));
-        consumer.accept(Component.literal("   modify the item transform."));
     }
 
 }
--- common/src/main/java/itemtransformhelper/StartupClientOnly.java
+++ common/src/main/java/itemtransformhelper/StartupClientOnly.java
@@ -1,6 +1,8 @@
 package itemtransformhelper;
 
 import dev.architectury.event.events.client.ClientTickEvent;
+import dev.architectury.event.events.client.ClientTooltipEvent;
+import net.minecraft.network.chat.Component;
 
 /**
  * User: The Grey Ghost
@@ -22,6 +24,15 @@ public class StartupClientOnly {
 
     public static void clientSetup() {
         ClientTickEvent.CLIENT_PRE.register(mc -> clientTickHandler.clientTickEvent());
+
+        ClientTooltipEvent.ITEM.register((stack, lines, tooltipContext, flag) -> {
+            if (stack.is(StartupCommon.ITEM_CAMERA.get())) {
+                lines.addLast(Component.literal("1) Place the camera in your hotbar"));
+                lines.addLast(Component.literal("2) Hold an item in your hand"));
+                lines.addLast(Component.literal("3) Use the cursor keys to"));
+                lines.addLast(Component.literal("   modify the item transform."));
+            }
+        });
     }
 
 }
